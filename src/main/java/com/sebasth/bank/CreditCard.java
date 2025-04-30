518c6ee33b1cc1be9ba235bc01244998deba44d5
sebasth.turflor <sebasthsegura05@gmail.com>
2025-04-30 09:18:55 -0500

    objeto tarjeta de credito y sus metodos ademas de los getters y setters

---
+++ src/main/java/com/sebasth/bank/CreditCard.java
@@ -0,0 +1,47 @@
+package com.sebasth.bank;
+
+public class CreditCard {
+    private String number;
+    private double limit;
+    private double balance;
+    private String expirateDate;
+
+    public CreditCard(String number, String expirateDate){
+        this.number = number;
+        this.expirateDate = expirateDate;
+        this.limit = 100;
+        this.balance = 100;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getExpirateDate() {
+        return expirateDate;
+    }
+
+    public void setExpirateDate(String expirateDate) {
+        this.expirateDate = expirateDate;
+    }
+
+    public boolean makePurchase(double amount){
+        if(balance >= amount){
+            limit -= amount;
+            return true;
+        }
+        return false;
+    }
+}
