c235f09256cdb0d8e0e7433ed4f2a0ac12a174ed
sebasth.turflor <sebasthsegura05@gmail.com>
2025-05-07 13:12:00 -0500

    update objetc Authentication

--- src/main/java/com/sebasth/bank/objects/Authentication.java
+++ src/main/java/com/sebasth/bank/objects/Authentication.java
@@ -1,10 +1,71 @@
 package com.sebasth.bank.objects;
 
+import java.util.Objects;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Authentication {
-    private int userId;
+    private long userId;
     private String userName;
-    private Date lastLogin;
+    private String hashedPassword;
+    private LocalDateTime lastLogin;
     private String password;
+    private Boolean isActive;
+
+    /*constructor*/
+    public Authentication(){
+
+    }
+
+    /*creamos su metodo de verficacion y obtencion de datos*/
+    public Authentication(long userId, String userName, String hashedPassword){
+        this.userId = userId;
+        this.userName = userName;
+        this.hashedPassword = hashedPassword;
+        this.isActive = true;
+        this.lastLogin = null;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+    /*creamos metodos adicionales*/
+
+
 }
