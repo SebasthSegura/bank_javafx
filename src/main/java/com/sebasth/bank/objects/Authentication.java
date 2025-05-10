09edead7446a14c3df8503178b6d3a4098517ad4
JuanSegura <sebasthsegura05@gmail.com>
2025-05-10 11:47:44 -0500

    update Authentication

--- src/main/java/com/sebasth/bank/objects/Authentication.java
+++ src/main/java/com/sebasth/bank/objects/Authentication.java
@@ -67,5 +67,36 @@ public class Authentication {
     }
     /*creamos metodos adicionales*/
 
+    public boolean checkPassword(String plainPassword) {
+        return Objects.equals((this.hashedPassword), plainPassword);
+    }
+    /*
+    Actualizar la fecha y hora del ultimo inicio de sesion
+    */
+    public void updateLastLogin() {
+        this.lastLogin = LocalDateTime.now(java.time.Clock.system(java.time.ZoneId.of("America/Bogota")));
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authentication that = (Authentication) o;
+        return  Objects.equals(userId, that.userId) && Objects.equals(userName, that.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId,userName);
+    }
+
+    @Override
+    public String toString() {
+        return "Authentication{" +
+                "userId=" + userId +
+                ", userNAme='" + userName + '\'' +
+                ",lastLogin=" + lastLogin +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
