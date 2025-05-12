94bcfdae1426be16914087c176973e74fff4b8b1
JuanSegura <sebasthsegura05@gmail.com>
2025-05-12 13:38:17 -0500

    creating method's and builders to CreditCard.java && Wallet.java

--- src/main/java/com/sebasth/bank/objects/CreditCard.java
+++ src/main/java/com/sebasth/bank/objects/CreditCard.java
@@ -1,16 +1,43 @@
 package com.sebasth.bank.objects;
 
-public class CreditCard {
-    private String number;
-    private double limit;
-    private double balance;
-    private String expirateDate;
+import java.util.Objects;
 
-    public CreditCard(String number, String expirateDate){
+public class CreditCard {
+    private Long cardId;
+    private String number;
+    private double creditLimit;
+    private double aviableBalance;
+    private String expirateDate;
+    private long userId;
+    private String cardType;
+
+
+    /*agregamos los constructores (builders)*/
+    public CreditCard(String number, String expirateDate, String cardType){
         this.number = number;
         this.expirateDate = expirateDate;
-        this.limit = 100;
-        this.balance = 100;
+        this.cardType = cardType;
+        this.creditLimit = 100.0;
+        this.aviableBalance = 100.0;
+    }
+
+    public CreditCard(Long cardId, String number, double creditLimit, double aviableBalance, String expirateDate, Long userId, String cardType){
+        this.cardId = cardId;
+        this.number = number;
+        this.creditLimit = creditLimit;
+        this.aviableBalance = aviableBalance;
+        this.expirateDate = expirateDate;
+        this.userId = userId;
+        this.cardType = cardType;
+    }
+
+    /*ageregamos los getters y setters*/
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Long cardId) {
+        this.cardId = cardId;
     }
 
     public String getNumber() {
@@ -21,12 +48,20 @@ public class CreditCard {
         this.number = number;
     }
 
-    public double getBalance() {
-        return balance;
+    public double getCreditLimit() {
+        return creditLimit;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public double getAviableBalance() {
+        return aviableBalance;
+    }
+
+    public void setAviableBalance(double aviableBalance) {
+        this.aviableBalance = aviableBalance;
     }
 
     public String getExpirateDate() {
@@ -37,11 +72,64 @@ public class CreditCard {
         this.expirateDate = expirateDate;
     }
 
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    /*metodos de las tarjetas comprar y pagar*/
     public boolean makePurchase(double amount){
-        if(balance >= amount){
-            limit -= amount;
+        if(aviableBalance >= amount){
+            creditLimit -= amount;
             return true;
         }
         return false;
+    }
+
+    public void makePayment(double amount){
+        if(amount > 0){
+            aviableBalance += amount;
+            if(aviableBalance > creditLimit){
+                aviableBalance = creditLimit;
+            }
+        }
+    }
+
+    /*sobre escribimos los metodos equals, hash y toString para verificar si la tarjeta es valida*/
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreditCard that = (CreditCard) o;
+        return Double.compare(that.creditLimit, creditLimit) == 0 && Double.compare(that.aviableBalance, aviableBalance) == 0 && Objects.equals(number,that.number) && Objects.equals(expirateDate, that.expirateDate) && Objects.equals(userId,that.userId) && Objects.equals(cardType, that.cardType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardId, number, creditLimit, aviableBalance, expirateDate, userId, cardType);
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "cardId=" + cardId +
+                ", number='" + number + '\'' +
+                ", creditLimit=" + creditLimit +
+                ", aviableBalance=" + aviableBalance +
+                ", expirateDate='" + expirateDate + '\'' +
+                ", userId=" + userId +
+                ", cardType='" + cardType + '\'' +
+                '}';
     }
 }
--- src/main/java/com/sebasth/bank/objects/Wallet.java
+++ src/main/java/com/sebasth/bank/objects/Wallet.java
@@ -1,21 +1,43 @@
 package com.sebasth.bank.objects;
 
+import java.util.Objects;
+
 public class Wallet {
     private String accountID;
+    private Long walletId;
     private double balance;
     private double limit;
+    private Long userId;
 
+    /*creamos un constructor generico que dara el saldo inicial de la billetera*/
     public Wallet(double initialbalance){
         this.balance = initialbalance;
     }
 
+    /*creamos un constructor mas completo*/
+    public Wallet(Long walletId, String accountID, double balance, double limit, long userId){
+        this.walletId = walletId;
+        this.accountID = accountID;
+        this.balance = balance;
+        this.limit = limit;
+        this.userId = userId;
+    }
 
+    /*agregamos los getters y setters*/
     public String getAccountID() {
         return accountID;
     }
 
     public void setAccountID(String accountID) {
         this.accountID = accountID;
+    }
+
+    public Long getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(Long walletId) {
+        this.walletId = walletId;
     }
 
     public double getBalance() {
@@ -25,15 +47,61 @@ public class Wallet {
     public void setBalance(double balance) {
         this.balance = balance;
     }
-    public void deposit(double amount){
-        balance += amount;
+
+    public double getLimit() {
+        return limit;
     }
 
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    /*agregamos los metodos para gestionar la billetera (retirar y depositar)*/
     public boolean withdraw(double amount){
         if(balance >= amount){
             balance -= amount;
             return true;
         }
         return false;
+    }
+    public boolean deposit(double amount){
+        if (amount > 0 && amount <= limit){
+            balance += amount;
+            return true;
+
+        }else {
+            return false;
+        }
+    }
+    /*sobreescribimos los metodos equals, hashCode y toString para verificar si la billetera es valida*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountID, walletId, balance, limit, userId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null ||getClass() != o.getClass()) return false;
+        Wallet wallet = (Wallet) o;
+        return Double.compare(wallet.balance, balance) == 0 && Double.compare(wallet.limit, limit) == 0 && Objects.equals(accountID, wallet.accountID) && Objects.equals(walletId, wallet.walletId) && Objects.equals(userId, wallet.userId);
+    }
+
+    @Override
+    public String toString() {
+        return "wallet{" +
+                "accountID=" + accountID
+                + ", walletId=" + walletId
+                + ", balance=" + balance + ", limit="
+                + limit + ", userId=" + userId + '}';
     }
 }
