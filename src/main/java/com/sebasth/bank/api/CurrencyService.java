6422894e074e09a31b62c9f2664b72634c0626d9
JuanSegura <sebasthsegura05@gmail.com>
2025-05-17 08:25:15 -0500

    fix up CurrencyService && add method getExchangeRate

--- src/main/java/com/sebasth/bank/api/CurrencyService.java
+++ src/main/java/com/sebasth/bank/api/CurrencyService.java
@@ -5,8 +5,10 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Iterator;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.print.DocFlavor;
@@ -32,7 +34,8 @@ public class CurrencyService {
                                 JSONObject rates = jsonResponse.getJSONObject("rates");
                                 // Imprimir las tasas de cambio
                                 System.out.println("Exchange rates for " + baseCurrency + ":");
-                                for (String currencyCode : rates.keySet()) {
+                                for (Iterator<String> it = rates.keys(); it.hasNext(); ) {
+                                    String currencyCode = it.next();
                                     System.out.println(currencyCode + ": " + rates.getDouble(currencyCode));
                                 }
                             } else {
@@ -41,7 +44,7 @@ public class CurrencyService {
                         } else {
                             System.out.println("Error: " + response.statusCode());
                         }
-                    } catch (org.json.JSONException e) {
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 })
@@ -50,4 +53,34 @@ public class CurrencyService {
                     return null;
                 });
     }
+    public void getExchangeRate(String baseCurrency, String targetCurrency){
+        String url = BASE_API + API_KEY + "/pair/" + baseCurrency + "/" + targetCurrency;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenAccept(response -> {
+                    try {
+                        if (response.statusCode() == 200) {
+                            String responseBody = response.body();
+                            JSONObject jsonResponse = new JSONObject(responseBody);
+                            if ("success".equals(jsonResponse.getString("result"))) {
+                                double rate = jsonResponse.getDouble("conversion_rate");
+                                System.out.println("Exchange rate from " + baseCurrency + " to " + targetCurrency + ": " + rate);
+                            } else {
+                                System.out.println("Error: " + jsonResponse.getString("error-type"));
+                            }
+                        } else {
+                            System.out.println("Error: " + response.statusCode());
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                })
+                .exceptionally(e -> {
+                    e.printStackTrace();
+                    return null;
+                });
+        }
 }
