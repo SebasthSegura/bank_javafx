e329a6bf1aae6da0cdf0ed0a5b579ded2e228953
JuanSegura <sebasthsegura05@gmail.com>
2025-05-24 09:54:15 -0500

    create connection data base

--- src/main/java/com/sebasth/bank/database/DataBaseConnection.java
+++ src/main/java/com/sebasth/bank/database/DataBaseConnection.java
@@ -1,4 +1,41 @@
 package com.sebasth.bank.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    private String jbcUrl;
+    private String username;
+    private String password;
+
+    public DataBaseConnection(String jbcUrl, String username, String password) {
+        this.jbcUrl = jbcUrl;
+        this.username = username;
+        this.password = password;
+    }
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(jbcUrl, username, password);
+    }
+    public void CloseConnection(Connection connection){
+        if(connection != null){
+            try {
+                connection.close();
+                System.out.println("Connection closed successfully.");
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e.getMessage());
+            }
+        }
+    }
+    public void rollbackTransaction(Connection connection){
+        if (connection != null){
+            try {
+                connection.rollback();
+                System.out.println("Transaction rolled bacck successfully.");
+            } catch (SQLException e) {
+                System.out.println("Error rolling back transaction: " + e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module com.sebasth.bank {
     requires org.json.chargebee;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
+    requires java.sql;
 
     opens com.sebasth.bank to javafx.fxml;
     exports com.sebasth.bank;
